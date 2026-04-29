6d6fb251c7b2dee34cb32a4e086dcadd0d5877b3
Helber <helber.educ@proton.me>
2026-04-29 14:33:08 -0300

    feat: add CRUD APIs

---
+++ backend/src/controllers/post.controller.js
@@ -0,0 +1,90 @@
+import { Post } from "../models/post.model.js";
+
+// Create a post
+const createPost = async (req, res) => {
+  try {
+    const { name, description, age } = req.body;
+
+    if (!name || !description || !age) {
+      return res.status(400).json({
+        message: "All field are required",
+      });
+    }
+
+    const post = await Post.create({ name, description, age });
+
+    res.status(201).json({
+      message: "Post created successfully",
+      post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
+
+//  Read all posts
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
+
+// Update post
+const updatePost = async (req, res) => {
+  try {
+    // basic validation checking if body is empty
+    if (Object.keys(req.body).lenght === 0) {
+      return res.status(400).json({
+        message: "No data provider for update",
+      });
+    }
+
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!post)
+      return res.status(404).json({
+        message: "Post not found",
+      });
+
+    res.status(200).json({
+      message: "Post Updated successfully",
+      post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const deleted = await Post.findByIdAndDelete(req.params.id);
+    if (!deleted)
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    res.status(200).json({
+      message: "Post successfully deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
+
+export { createPost, getPosts, updatePost, deletePost };
